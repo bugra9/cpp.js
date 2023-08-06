d271ae24c41bc65ccfa73abc01a0142f972426bb
Bugra <git.bugra@gmail.com>
2023-08-06 21:06:52 +0300

    fix(cppjs-create-cpp.js): bind samples folder

---
+++ samples/cppjs-sample-lib-complex/cpp.js
@@ -0,0 +1,11 @@
+import * as url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const temp = __filename.split('/'); temp.pop();
+const __dirname = temp.join('/');
+
+export default {
+    path: __dirname,
+    headers: [`${__dirname}/include`],
+    libs: [`${__dirname}/lib`],
+}
