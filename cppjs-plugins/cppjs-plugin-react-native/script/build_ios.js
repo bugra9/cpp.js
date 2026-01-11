fcb755a342b9f6be768380c95fc61aecab1dc7c9
Bugra SARI <git.bugra@gmail.com>
2026-01-11 19:40:39 +0300

    .

--- cppjs-plugins/cppjs-plugin-react-native/script/build_ios.js
+++ cppjs-plugins/cppjs-plugin-react-native/script/build_ios.js
@@ -70,7 +70,7 @@ const overrideConfig = {
         platform: 'ios',
         arch: 'all',
         runtime: 'mt',
-        buildType: buildType.toLowerCase(),
+        buildType: buildTargetIPhoneOS.buildType.buildType,
     }
 };
 createXCFramework(overrideConfig);
